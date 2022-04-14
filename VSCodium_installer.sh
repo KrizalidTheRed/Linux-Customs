7bde478bfb2e0ed3e9e99f5adf0aa9fccb224598
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-04-14 15:21:11 -0400

    Add files via upload

---
+++ VSCodium_installer.sh
@@ -0,0 +1,14 @@
+figlet VSCodium Installer
+toilet -f future by Erick Nunes
+
+echo "\n\n\n\n"
+
+wget https://github.com/VSCodium/vscodium/releases/download/1.66.2/codium_1.66.2-1649809017_amd64.deb
+
+sudo chmod 755 codium_1.66.2-1649809017_amd64.deb
+
+sudo dpkg -i codium_1.66.2-1649809017_amd64.deb
+
+rm -rf codium_1.66.2-1649809017_amd64.deb
+
+
