f73e7b14692760748863a62c08f5dc64e76923ba
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-05-24 09:18:44 -0300

    Update rtl88x2bu_tl-wn821n_TP_LINK.sh

--- rtl88x2bu_tl-wn821n_TP_LINK.sh
+++ rtl88x2bu_tl-wn821n_TP_LINK.sh
@@ -1,6 +1,7 @@
 echo "Welcome to rtl88x2bu installer"
-
-sudo apt install git dmks -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git dkms -y
 
 git clone https://github.com/cilynx/rtl88x2bu.git
 cd rtl88x2bu
