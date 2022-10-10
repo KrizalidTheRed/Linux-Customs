8ca47923bcc0d9f809ba6ae17b3c92ceabbe3350
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-10-10 10:38:18 -0300

    Update rtl88x22BU_kali.sh

--- rtl88x22BU_kali.sh
+++ rtl88x22BU_kali.sh
@@ -1,4 +1,4 @@
-sudo apt install git build-essential linux-headers-$(uname -r)
+sudo apt install git build-essential linux-headers
 git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver.git
 cd RTL88x2BU-Linux-Driver
 make 
